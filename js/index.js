9e24b938b5399998c874f0bd2002c5cbe3a34cdd
Dheeraj Khardwal <dj@nevitus.com>
2015-06-07 23:16:49 +0530

    Launching SailingWords Homepage

--- js/index.js
+++ js/index.js
@@ -4,47 +4,73 @@ tim.index = {};
 
 // ready event
 tim.index.ready = function() {
-    
-    var $header     = $('.masthead'),
-        $ui         = $header.find('.typer'),
-        $library    = $header.find('.library'),
-        $version    = $header.find('.version'),
-        $logo       = $header.find('.container'),
-        handler     = {
-            introduction: function() {
-                // zoom out
-                setTimeout(function() {
-                    $header
-                    .removeClass('zoomed')
-                    ;
-                }, 14000);
-                
-                $ui.typed({
-                    replaceBaseText : true,
-                    strings         : [
-                        "Silence is the power of gods.",
-                        "Words are ours.",
-                        "Words can build bridges across darkness and chaos.",
-                        "<b>Simplify</b> life with words, with Us."
-                    ],
-                    showCursor      : false,
-                    typeSpeed       : 50,
-                    backSpeed       : 20,
-                    backDelay       : 200
-                });
-                
-                setTimeout(function() {
-                    $library.transition('scale in', 1000);
-                    $logo.transition('fade', 5000);
-                }, 16500);
-                
-                setTimeout(function() {
-                    $version.transition('fade', 1000);
-                }, 17100);
-            }
-        };
-    
-    handler.introduction();
+    $('#slides').superslides({
+        animation: 'fade',
+        slide_easing: 'easeInOutCubic',
+        slide_speed: 800,
+        pagination: false,
+        hashchange: false,
+        scrollable: true//,
+        //play: 5000
+    });
+
+    $('#testimonial-slides').superslides({
+        slide_easing: 'easeInOutCubic',
+        slide_speed: 2000,
+        pagination: false,
+        hashchange: false,
+        scrollable: true,
+        inherit_width_from: '#testimonial-slides',
+        inherit_height_from: '#testimonial-slides'//,
+        //play: 9000
+    });
+
+    function filterPath(string) {
+        return string
+            .replace(/^\//,'')
+            .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+            .replace(/\/$/,'');
+    }
+    
+    var locationPath = filterPath(location.pathname);
+    var scrollElem = scrollableElement('html', 'body');
+ 
+    $('a[href*=#]').each(function() {
+        var thisPath = filterPath(this.pathname) || locationPath;
+        if (  locationPath == thisPath
+            && (location.hostname == this.hostname || !this.hostname)
+            && this.hash.replace(/#/,'') ) {
+            var $target = $(this.hash), target = this.hash;
+            if (target) {
+                var targetOffset = $target.offset().top;
+                $(this).click(function(event) {
+                    event.preventDefault();
+                    $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+                        location.hash = target;
+                    });
+                });
+            }
+        }
+    });
+ 
+      // use the first element that is "scrollable"
+      function scrollableElement(els) {
+        for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+          var el = arguments[i],
+              $scrollElement = $(el);
+          if ($scrollElement.scrollTop()> 0) {
+            return el;
+          } else {
+            $scrollElement.scrollTop(1);
+            var isScrollable = $scrollElement.scrollTop()> 0;
+            $scrollElement.scrollTop(0);
+            if (isScrollable) {
+              return el;
+            }
+          }
+        }
+        return [];
+      }
 };
 
 // attach ready event
